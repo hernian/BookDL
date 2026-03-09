98b740e0acc85d1122b174ff5717a1a8c5efcfb5
hernian <hernianrunner@gmail.com>
2026-03-09 19:01:54 +0900

    プロジェクトファイルを追加。

---
+++ BookDL/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HernianLib.Controls;
+
+namespace BookDL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WebView2Contorl _webViewControl;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _webViewControl = new WebView2Contorl(webView);
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+            await _webViewControl.InitializeWebViewAsync();
+            this.IsEnabled = true;
+            await Task.Delay(5000);
+            var result = await _webViewControl.NavigateAsync("https://www.google.com");
+            Debug.WriteLine($"Navigation result: IsSuccess={result.IsSuccess}, HttpStatusCode={result.HttpStatusCode}, WebErrorStatus={result.WebErrorStatus}");
+        }
+    }
+}
---
+++ HernianLib/Controls/WebView2Contorl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Web.WebView2.Wpf;      // WebView2 コントロール本体
+using Microsoft.Web.WebView2.Core;     // CoreWebView2, 設定、イベント引数など
+
+
+namespace HernianLib.Controls
+{
+    public class WebView2Contorl
+    {
+        public record NavigationResult(bool IsSuccess, int HttpStatusCode, CoreWebView2WebErrorStatus WebErrorStatus);
+
+        private readonly WebView2 _webView;
+        private TaskCompletionSource<NavigationResult>? _navigationTcs;
+
+        public WebView2Contorl(WebView2 webView)
+        {
+            _webView = webView;
+            _webView.NavigationCompleted += WebView_NavigationCompleted;
+        }
+
+        public async Task InitializeWebViewAsync()
+        {
+            _webView.IsEnabled = false;
+            await _webView.EnsureCoreWebView2Async();
+            _webView.IsEnabled = true;
+        }
+
+        private void WebView_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (_navigationTcs != null)
+            {
+                _navigationTcs.TrySetResult(new NavigationResult(e.IsSuccess, e.HttpStatusCode, e.WebErrorStatus));
+                _navigationTcs = null;
+            }
+        }
+
+        public Task<NavigationResult> NavigateAsync(string url)
+        {
+            if (_navigationTcs != null)
+            {
+                throw new InvalidOperationException("前のナビゲーションが完了していません。");
+            }
+            _navigationTcs = new TaskCompletionSource<NavigationResult>();
+            _webView.CoreWebView2.Navigate(url);
+            return _navigationTcs.Task;
+        }
+    }
+}
